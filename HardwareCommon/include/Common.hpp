f2556b075b7a75e69ec43af5f856f13930998af3
Donovan Maas <donovan.maas@grafikastrahlen.dev>
2025-06-07 16:25:12 -0500

    Improved VHDL macros
    There are a lot of changes in this one.
    First, some helpers for process calls were added. These had to be added because some processes could trigger changes that necessitated other processes. This could create an infinite loop that would stall the program.
    Second, the Sensitivity enum is now defined with a macro, thus allows us to ensure it is always named correctly.
    Third, the names of the enum members should now be the actual names of the variables. This will make more sense shortly, but it makes it easier to see the analogue VHDL code.
    With the change to the validating triggers for processes it also became advantageous to have some more macros.
    The `PROCESSES_DECL` macro now declares the generic `Processes` function that most entities would have. This allows us to ensure that the function and the `trigger` parameter are correctly named.
    Processes are now declared with their own macro: `PROCESS_DECL`, this again ensures that the `trigger` parameter is correctly named.
    Calling the `Processes` is now done with a macro: `TRIGGER_SENSITIVTY`. This has 2 purposes: first, the name of `Processes` is now opaque to the entity; second we can now omit the `Sensitivity` enum name. This is where naming the enum members the same as the entity members starts to become advantageous. It will now be much clearer what is being triggered.
    A similar effect is now had for the `RisingEdge` and `FallingEdge` functions. Where before you had to pass the enum name, the variable, and the trigger sense, we can now simply call `RISING_EDGE(var)`. Because the name for `trigger` is now opaque, we can guarantee it is always what we want, and because the enum members and entity members are named identically, it is trivial to pass them both with a single macro parameter.
    There are also some equivalent functions for when the signal is actually mux. The only difference is this calls the signal mux function rather than passing it as a variable.
    Entering Processes from the `Processes` function is now simplified. With these changes it became necessary to always wrap the call in an if to check for the triggers, now we can do that with a single macros that will call the Process if the trigger was correct. This unfortunately requires a lot of helper macros, but at least it works.
    As much as I don't like macros, these make it much quicker to write out code (eliminating a lot of boilerplate chars), as well as easing understanding since you can now clearly see the relation between a signal and how it triggers a process.

--- HardwareCommon/include/Common.hpp
+++ HardwareCommon/include/Common.hpp
@@ -33,8 +33,31 @@ template<typename Sensitivity, Sensitivity TargetSense>
     return !bit && EventVHDL<Sensitivity, TargetSense>(sensitivity);
 }
 
+template<typename Sensitivity>
+[[nodiscard]] bool ProcessVHDL(const Sensitivity trigger) noexcept
+{
+    return false;
+}
+
+template<typename Sensitivity, Sensitivity TargetSense, Sensitivity... RemainingSenses>
+[[nodiscard]] bool ProcessVHDL(const Sensitivity trigger) noexcept
+{
+    if(trigger == TargetSense)
+    {
+        return true;
+    }
+
+    return ProcessVHDL<Sensitivity, RemainingSenses...>(trigger);
+}
+
 // Lots of stuff needs the main processor, so we'll just keep it defined here.
 class Processor;
+
+#define SENSITIVITY_DECL(...) \
+    enum class Sensitivity : u8                                                                         \
+    {                                                                                                   \
+        __VA_ARGS__                                                                                     \
+    }
 
 #define SIGNAL_ENTITIES() \
     template<Sensitivity TargetSense>                                                                   \
@@ -53,4 +76,66 @@ class Processor;
     [[nodiscard]] bool FallingEdge(const u32 bit, const Sensitivity sensitivity) noexcept               \
     {                                                                                                   \
         return FallingEdgeVHDL<Sensitivity, TargetSense>(bit, sensitivity);                             \
+    }                                                                                                   \
+                                                                                                        \
+    template<Sensitivity TargetSense, Sensitivity... RemainingSenses>                                   \
+    [[nodiscard]] bool Process(const Sensitivity trigger) noexcept                                      \
+    {                                                                                                   \
+        if(trigger == TargetSense)                                                                      \
+        {                                                                                               \
+            return true;                                                                                \
+        }                                                                                               \
+                                                                                                        \
+        return ProcessVHDL<Sensitivity, RemainingSenses...>(trigger);                                   \
     }
+
+#define PROCESSES_DECL() \
+    void Processes(const Sensitivity trigger) noexcept
+
+#define PROCESS_DECL(Name) \
+    void Name(const Sensitivity trigger) noexcept
+
+#define TRIGGER_SENSITIVITY(Sense) \
+    Processes(Sensitivity::Sense)
+
+#define RISING_EDGE(Sense) \
+    RisingEdge<Sensitivity::Sense>(Sense, trigger)
+
+#define FALLING_EDGE(Sense) \
+    FallingEdge<Sensitivity::Sense>(Sense, trigger)
+
+#define RISING_EDGE_MUX(Sense) \
+    RisingEdge<Sensitivity::Sense>(Sense(), trigger)
+
+#define FALLING_EDGE_MUX(Sense) \
+    FallingEdge<Sensitivity::Sense>(Sense(), trigger)
+
+#define VHDL_INTERNAL_EXPAND(X) X
+#define VHDL_INTERNAL_CAT(A, B) A ## B
+#define VHDL_INTERNAL_SELECT(NAME, NUM) VHDL_INTERNAL_CAT(NAME, NUM)
+#define VHDL_INTERNAL_GET_COUNT(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
+#define VHDL_INTERNAL_VA_SIZE(...) VHDL_INTERNAL_EXPAND(VHDL_INTERNAL_GET_COUNT(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1))
+
+#define VHDL_SENSITIVITY_1(Sense1) Sensitivity::Sense1
+#define VHDL_SENSITIVITY_2(Sense1, Sense2) VHDL_SENSITIVITY_1(Sense1), VHDL_SENSITIVITY_1(Sense2)
+#define VHDL_SENSITIVITY_3(Sense1, Sense2, Sense3) VHDL_SENSITIVITY_2(Sense1, Sense2), VHDL_SENSITIVITY_1(Sense3)
+#define VHDL_SENSITIVITY_4(Sense1, Sense2, Sense3, Sense4) VHDL_SENSITIVITY_3(Sense1, Sense2, Sense3), VHDL_SENSITIVITY_1(Sense4)
+#define VHDL_SENSITIVITY_5(Sense1, Sense2, Sense3, Sense4, Sense5) VHDL_SENSITIVITY_4(Sense1, Sense2, Sense3, Sense4), VHDL_SENSITIVITY_1(Sense5)
+#define VHDL_SENSITIVITY_6(Sense1, Sense2, Sense3, Sense4, Sense5, Sense6) VHDL_SENSITIVITY_5(Sense1, Sense2, Sense3, Sense4, Sense5), VHDL_SENSITIVITY_1(Sense6)
+#define VHDL_SENSITIVITY_7(Sense1, Sense2, Sense3, Sense4, Sense5, Sense6, Sense7) VHDL_SENSITIVITY_6(Sense1, Sense2, Sense3, Sense4, Sense5, Sense6), VHDL_SENSITIVITY_1(Sense7)
+#define VHDL_SENSITIVITY_8(Sense1, Sense2, Sense3, Sense4, Sense5, Sense6, Sense7, Sense8) VHDL_SENSITIVITY_7(Sense1, Sense2, Sense3, Sense4, Sense5, Sense6, Sense7), VHDL_SENSITIVITY_1(Sense8)
+
+#define VHDL_SENSITIVITY_N_ARGS(N, ...) VHDL_INTERNAL_EXPAND(VHDL_INTERNAL_SELECT(VHDL_SENSITIVITY_, N)(__VA_ARGS__))
+#define VHDL_SENSITIVITY_ARGS(...) VHDL_SENSITIVITY_N_ARGS(VHDL_INTERNAL_VA_SIZE(__VA_ARGS__), __VA_ARGS__)
+
+#define PROCESS_BEGIN(...) \
+    if(Process<VHDL_SENSITIVITY_ARGS(__VA_ARGS__)>(trigger))
+
+#define PROCESS_ENTER(ProcessName, ...) \
+    PROCESS_BEGIN(__VA_ARGS__)                                                                          \
+    {                                                                                                   \
+        ProcessName(trigger);                                                                           \
+    }
+
+#define ENTER_PROCESS_M(Name) \
+    Name(trigger)
