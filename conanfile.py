39c32a57ed6be0c520a9258530d1da9f869dc4cc
Donovan Maas <donovan.maas@grafikastrahlen.dev>
2025-08-20 20:37:47 -0500

    Added CMake

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,13 @@
 from conan import ConanFile
 from conan.tools.microsoft import MSBuildDeps
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class SoftGpu(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
-    requires = "tauutils/[^1.3.10]"
+    requires = "tauutils/[^1.4.0]", "glm/[^1.0.1]", "vulkan-memory-allocator/[^3.0.1]"
+
+    def configure(self):
+        self.options["tauutils"].shared = False
 
     def generate(self):
         ms = MSBuildDeps(self)
@@ -11,7 +15,21 @@ class SoftGpu(ConanFile):
         if self.settings.build_type == "Debug":
             ms.configuration = "TestSetup"
             ms.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def layout(self):
-        self.folders.generators = "libs/conan"
+        cmake_layout(self)
+        # self.folders.generators = "libs/conan"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
 
--- init.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-conan install . -s build_type=Debug -o tauutils/*:shared=False
-conan install . -s build_type=Release -o tauutils/*:shared=False
