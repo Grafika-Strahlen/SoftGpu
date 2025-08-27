9e564e0ab03790e29ffc5ba193368be02a08a196
Donovan Maas <donovan.maas@grafikastrahlen.dev>
2025-08-27 15:49:27 -0500

    Improved TlpHeader format

--- HardwareCommon/include/PcieProtocol.hpp
+++ HardwareCommon/include/PcieProtocol.hpp
@@ -158,24 +158,30 @@ static_assert(sizeof(AdvancedErrorReportingCapabilityStructure) == 0x2C, "Advanc
 
 struct TlpHeader final
 {
-    static inline constexpr u32 FORMAT_3_DW_HEADER_NO_DATA   = 0b00;
-    static inline constexpr u32 FORMAT_4_DW_HEADER_NO_DATA   = 0b01;
-    static inline constexpr u32 FORMAT_3_DW_HEADER_WITH_DATA = 0b10;
-    static inline constexpr u32 FORMAT_4_DW_HEADER_WITH_DATA = 0b11;
+    enum EFormat : u32
+    {
+        FORMAT_3_DW_HEADER_NO_DATA   = 0b00,
+        FORMAT_4_DW_HEADER_NO_DATA   = 0b01,
+        FORMAT_3_DW_HEADER_WITH_DATA = 0b10,
+        FORMAT_4_DW_HEADER_WITH_DATA = 0b11,
+    };
 
-    static inline constexpr u32 TYPE_MEMORY_REQUEST             = 0b0'0000;
-    static inline constexpr u32 TYPE_MEMORY_READ_REQUEST_LOCKED = 0b0'0001;
-    static inline constexpr u32 TYPE_IO_REQUEST                 = 0b0'0010;
-    static inline constexpr u32 TYPE_CONFIG_TYPE_0_REQUEST      = 0b0'0100;
-    static inline constexpr u32 TYPE_CONFIG_TYPE_1_REQUEST      = 0b0'0101;
-    static inline constexpr u32 TYPE_MESSAGE                    = 0b1'0000;
-    static inline constexpr u32 TYPE_COMPLETION                 = 0b0'1010;
-    static inline constexpr u32 TYPE_COMPLETION_LOCKED_READ     = 0b0'1011;
+    enum EType : u32
+    {
+        TYPE_MEMORY_REQUEST             = 0b0'0000,
+        TYPE_MEMORY_READ_REQUEST_LOCKED = 0b0'0001,
+        TYPE_IO_REQUEST                 = 0b0'0010,
+        TYPE_CONFIG_TYPE_0_REQUEST      = 0b0'0100,
+        TYPE_CONFIG_TYPE_1_REQUEST      = 0b0'0101,
+        TYPE_MESSAGE                    = 0b1'0000,
+        TYPE_COMPLETION                 = 0b0'1010,
+        TYPE_COMPLETION_LOCKED_READ     = 0b0'1011,
+    };
 
 public:
     u32 Reserved0 : 1;
-    u32 Fmt : 2;
-    u32 Type : 5;
+    EFormat Fmt : 2;
+    EType Type : 5;
     u32 Reserved1 : 1;
     u32 TC : 3;
     u32 Reserved2 : 4;
